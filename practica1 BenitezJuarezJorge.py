cdfba6a4b248509238cee0f13e7b8ddec54204cf
Benitez-Juarez-Jorge <jorgebenitezjuarez15@gmail.com>
2020-03-21 14:29:49 -0600

    practica1
    ejercicios,

---
+++ practica1 BenitezJuarezJorge.py
@@ -0,0 +1,32 @@
+num= int(input("introduzca la cantidad de variables deseada:"))
+print("#1")
+for i in range(num+1):
+    print("*"*i)
+
+    
+num=int(input("introduzca la cantidad de variables deseada:"))    
+print("#2")
+for i in range (num,0,-1):
+    print("*"*i)
+
+
+num=int(input("introduzca la cantidad de variables deseada:"))
+print("#3")
+for i in range (1,num):
+    print("*"*i)
+for j in range (num,0,-1):
+    print("*"*j)
+    
+
+
+num=int(input("introduzca la cantidad de variables deseada:"))
+print("#4")
+for i in range (num):
+    print("*"*num)
+
+
+
+
+
+
+
